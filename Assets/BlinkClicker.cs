73604bbd9f799e7e96e34d2d068c863616ebdc90
Unknown <svenjanssen12321@gmail.com>
2020-09-01 17:04:52 +0200

    Added project & made a small prototype to test block sizes

---
+++ Assets/BlinkClicker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkClicker : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer SpriteRenderer;
+
+    private void Start()
+    {
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseDown()
+    {
+        print("clicked");
+        if (SpriteRenderer.color == Color.black)
+        {
+            print("color is black");
+            SpriteRenderer.color = Color.white;
+        }
+        else if (SpriteRenderer.color == Color.white)
+        {
+            print("color is white");
+            SpriteRenderer.color = Color.black;
+        }
+    }
+}
