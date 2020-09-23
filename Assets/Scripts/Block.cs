f33c76142b8e1936858eafd6ef88257cc8bd65f2
Unknown <svenjanssen12321@gmail.com>
2020-09-23 14:57:09 +0200

    worked on grid system

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Block : MonoBehaviour
+{
+    public Color Color;
+
+    [SerializeField] private SpriteRenderer SpriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetColor(Color c)
+    {
+        Color = c;
+        SpriteRenderer.color = c;
+    }
+}
---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private Row RowPrefab;
+    [SerializeField] private Block BlockPrefab;
+    [Space]
+    [SerializeField] private List<Color> BlockColors;
+    [Space]
+    [SerializeField] private Grid Grid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PopulateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void PopulateGrid()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            Row row = Instantiate(RowPrefab, transform);
+            row.transform.localPosition = new Vector3(0, -i, 0);
+            Grid.Rows.Add(row);
+
+            for (int j = 0; j < 7; j++)
+            {
+                Block block = Instantiate(BlockPrefab, row.transform.GetChild(j));
+                row.blocks.Add(block);
+
+                block.SetColor(GetRandomColor());
+            }
+        }
+    }
+
+    private Color GetRandomColor()
+    {
+        int rnd = Random.Range(0, BlockColors.Count);
+        return BlockColors[rnd];
+    }
+}
